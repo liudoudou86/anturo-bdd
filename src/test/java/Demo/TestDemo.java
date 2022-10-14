e7a2610346d78db688d53250faac8923affe8839
liudoudou86 <543972930@qq.com>
2022-10-14 11:04:48 +0800

    feat: 请求各种测试样例

--- src/test/java/Demo/TestDemo.java
+++ src/test/java/Demo/TestDemo.java
@@ -1,11 +1,12 @@
 package Demo;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 // 官方要求静态导入
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 /**
  * @author Tesla Liu
@@ -17,27 +18,60 @@ public class TestDemo {
 
     /**
      * Groovy风格写法，推荐使用这种方法
+     * 测试响应码
      */
     @Test
     public void testStatusCode() {
-        given().
-                get("https://www.baidu.com").
-                then().
-                statusCode(200);
+        given()
+                .get("https://jsonplaceholder.typicode.com/posts/3")
+                .then()
+                .statusCode(200)
+                .log().status();
     }
 
     /**
-     * Java原本风格写法
+     * 断言body结果
      */
     @Test
-    public void testStatusCodeJavaStyle() {
-        //1. 创建一个RestAssured对象
-        RestAssured ra = new RestAssured();
-        //2. 创建一个请求对象
-        RequestSpecification rs = ra.given();
-        //3. 发送请求，拿到响应对象
-        Response res = rs.get("https://www.baidu.com");
-        //4. 判断响应状态码是不是200
-        assert res.getStatusCode() == 200;
+    public void testBody() {
+        given()
+                .get("https://jsonplaceholder.typicode.com/posts/3")
+                .then()
+                .body("id",equalTo(3),"userId",equalTo(1))
+                .log().body();
+    }
+
+    /**
+     * 带param的请求
+     * */
+    @Test
+    public void testParam() {
+        given()
+                .param("userId", 2)
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then()
+                .statusCode(200)
+                .log().status();
+    }
+
+    /**
+     * 携带多个param的请求
+     * */
+    @Test
+    public void testWithParam() {
+        // 构造一个多参数的map对象
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("userId", "2");
+        parameter.put("id", "14");
+
+        given()
+                .params(parameter)
+                .when()
+                .log().all()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then()
+                .statusCode(200)
+                .log().status();
     }
 }
