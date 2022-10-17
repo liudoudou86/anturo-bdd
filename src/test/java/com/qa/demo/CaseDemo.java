badb5ff913b0e0ccb27f834998bd579c5b1f1dae
liudoudou86 <543972930@qq.com>
2022-10-17 17:15:19 +0800

    feat: 测试基类及用例测试

---
+++ src/test/java/com/qa/demo/CaseDemo.java
@@ -0,0 +1,16 @@
+package com.qa.demo;
+
+import com.qa.base.TestBase;
+import org.testng.annotations.Test;
+
+/**
+ * @author Tesla Liu
+ * @date 2022/10/17 17:14
+ * 描述
+ */
+public class CaseDemo extends TestBase {
+    @Test
+    public void test() {
+        logger.info("first test case");
+    }
+}
