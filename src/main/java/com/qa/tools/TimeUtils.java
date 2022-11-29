ed9dc703f94b540eaed9705f3f80a54611fed6da
liudoudou86 <543972930@qq.com>
2022-11-29 15:56:25 +0800

    feat:补充时间工具类

--- src/main/java/com/qa/tools/TimeUtils.java
+++ src/main/java/com/qa/tools/TimeUtils.java
@@ -1,31 +1,33 @@
 package com.qa.tools;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 /**
  * @author Tesla Liu
  * @date 2022/11/24 16:41
- * 描述 随机时间工具类
+ * 描述 时间工具类
  */
 public class TimeUtils {
 
-    public static String randomTimeStyle01(){
-        Random rndYear = new Random();
-        String year = String.valueOf(rndYear.nextInt(18)+2000);
-        Random rndMonth = new Random();
-        String month = String.valueOf(rndMonth.nextInt(12)+1);
-        Random rndDay = new Random();
-        String day = String.valueOf(rndDay.nextInt(30)+1);
-        Random rndHour = new Random();
-        String hour = String.valueOf(rndHour.nextInt(23));
-        Random rndMinute = new Random();
-        String minute = String.valueOf(rndMinute.nextInt(60));
-        Random rndSecond = new Random();
-        String second = String.valueOf(rndSecond.nextInt(60));
-        return year + month + day + hour + minute + second;
+    /**
+     * 月日时分秒补0
+     * */
+    private static String cp(String num){
+        String number = num + "";
+        if (number.length() == 1){
+            return "0" + number;
+        }else {
+            return number;
+        }
     }
 
-    public static String randomTimeStyle02(){
+    /**
+     * 获取随机时间戳
+     * 格式为：yyyyMMddHHmmss
+     * */
+    public static String randomTimeStamp(){
         Random rndYear = new Random();
         String year = String.valueOf(rndYear.nextInt(18)+2000);
         Random rndMonth = new Random();
@@ -38,7 +40,51 @@ public class TimeUtils {
         String minute = String.valueOf(rndMinute.nextInt(60));
         Random rndSecond = new Random();
         String second = String.valueOf(rndSecond.nextInt(60));
-        return year + "-" + month + "-" + day + "  " + hour + ":" + minute + ":" + second;
+        return year + cp(month) + cp(day) + cp(hour) + cp(minute) + cp(second);
+    }
+
+    /**
+     * 获取随机日期
+     * 格式为：yyyy-MM-dd
+     * */
+    public static String randomDate(){
+        Random rndYear = new Random();
+        String year = String.valueOf(rndYear.nextInt(18)+2000);
+        Random rndMonth = new Random();
+        String month = String.valueOf(rndMonth.nextInt(12)+1);
+        Random rndDay = new Random();
+        String day = String.valueOf(rndDay.nextInt(30)+1);
+        return year + "-" + cp(month) + "-" + cp(day);
+    }
+
+    /**
+     * 获取随机时间
+     * 格式为：yyyy-MM-dd HH:mm:ss
+     * */
+    public static String randomTime(){
+        Random rndYear = new Random();
+        String year = String.valueOf(rndYear.nextInt(18)+2000);
+        Random rndMonth = new Random();
+        String month = String.valueOf(rndMonth.nextInt(12)+1);
+        Random rndDay = new Random();
+        String day = String.valueOf(rndDay.nextInt(30)+1);
+        Random rndHour = new Random();
+        String hour = String.valueOf(rndHour.nextInt(23));
+        Random rndMinute = new Random();
+        String minute = String.valueOf(rndMinute.nextInt(60));
+        Random rndSecond = new Random();
+        String second = String.valueOf(rndSecond.nextInt(60));
+        return year + "-" + cp(month) + "-" + cp(day) + "  " + cp(hour) + ":" + cp(minute) + ":" + cp(second);
+    }
+
+    /**
+     * 获取当前时间
+     * 格式为：yyyy-MM-dd
+     * */
+    public static String currentTime() {
+        Date date = new Date();
+        SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
+        return formater.format(date);
     }
 
 }
